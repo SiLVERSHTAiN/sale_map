df5ef955f46e768e2d97d35c6f416e765c187ce0
SiLVERSHTAiN <silvershtain@protonmail.com>
2026-02-03 17:00:27 +0400

    check_buttons

--- src/keyboards.js
+++ src/keyboards.js
@@ -2,7 +2,7 @@ import { Markup } from "telegraf";
 
 export function mainMenuKeyboard() {
     return Markup.inlineKeyboard([
-        [Markup.button.webApp("🗺 Открыть витрину", "https://YOUR_DOMAIN/index.html")],
+        [Markup.button.webApp("🗺 Открыть витрину", "https://silvershtain.github.io/sale_map/")],
         [Markup.button.callback("✅ Бесплатный mini-гайд (.kmz)", "GET_MINI")],
         [Markup.button.callback("⭐ Купить полный путеводитель (Stars)", "BUY_FULL")],
         [Markup.button.callback("❓ Как установить (инструкция)", "HOW_TO")],
