295c191ec54e76749f7d015dae6e6175e4ffee0c
SiLVERSHTAiN <silvershtain@protonmail.com>
2026-02-07 00:25:07 +0400

    Set API base URL

--- docs/config.js
+++ docs/config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
     // URL вашего API на JustRunMy (без завершающего слеша)
     // Пример: "https://your-app.justrunmy.app"
-    API_BASE: ""
+    API_BASE: "https://sale_maps_c8d.a.jrnm.app"
 };
